743edbb9ad3f64ef66aa532e466c9fc52a4d1f7e
Loz <loz@the-pub.org>
2015-02-14 21:19:07 +0000

    dropping tables if exist

--- src/main/sql/create_tables.sql
+++ src/main/sql/create_tables.sql
@@ -1,3 +1,4 @@
+drop table if exists foodfest.EVENT;
 create table foodfest.EVENT (
       ID NUMERIC(20,0) not null,
       NAME varchar(255) null,
@@ -6,6 +7,7 @@ create table foodfest.EVENT (
       START_TIME TIMESTAMP null,
       END_TIME TIMESTAMP null
 );
+drop table if exists foodfest.TWEET;
 create table foodfest.TWEET (
       ID NUMERIC(20,0) not null,
       NAME varchar(255) null,
@@ -13,6 +15,7 @@ create table foodfest.TWEET (
       TEXT BLOB null,
       CREATED_DATE TIMESTAMP not null
 );
+drop table if exists foodfest.LAST_REFRESH;
 create table foodfest.LAST_REFRESH (
 	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	TABLE_NAME varchar(255) not null,
