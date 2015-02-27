c2a9a4cffa382f0012c6f43252c984ce13aaa51d
Loz <loz@the-pub.org>
2015-02-27 16:48:20 +0000

    Tidying old stuff

--- src/main/java/com/loz/EventAppConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.loz;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-
-
-
