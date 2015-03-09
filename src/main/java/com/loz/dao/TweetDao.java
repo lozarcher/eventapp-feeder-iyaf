713aa8ddfbccf5eb8f27b0e1a9c8b961c505dca4
Loz Archer <larcher@channel4.co.uk>
2015-03-09 17:12:26 +0000

    Reversing order of tweets

--- src/main/java/com/loz/dao/TweetDao.java
+++ src/main/java/com/loz/dao/TweetDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TweetDao extends CrudRepository<TweetData, Long> {
-    @Query("from TweetData order by createdDate asc")
+    @Query("from TweetData order by createdDate desc")
     List<TweetData> findAllOrderByDate();
 }
 
