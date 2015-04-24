76713711291f0f653bf8c68826166fe6ce679b64
Loz <loz@the-pub.org>
2015-04-24 17:02:36 +0100

    Bigger profile pictures in feed; added vouchers to SQL

--- src/main/sql/03_create_data.sql
+++ src/main/sql/03_create_data.sql
@@ -1,11 +1,27 @@
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (6, 'Doner', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/Doner.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (2, 'Greggs', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/Greggs.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (1, 'HSBC', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/HSBC.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (4, 'M&S', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/MandS.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (5, 'Maypole', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/Maypole.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (7, 'Wetherspoons', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/Spoons.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (8, 'Surbiton Plaza', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/SurbitonPlaza.png');
-INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (3, 'Waitrose', 'https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-725924976627/vouchers/Waitrose.png');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (1, 'Da Lucio', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher1.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (2, 'Naturally Chinese', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher2.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (3, 'C.F & M.C Stear', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher3.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (4, 'Laithwaite''s Wine', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher4.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (5, 'The Italian Taste', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher5.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (6, 'Main Kitchen Supplies', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher6.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (7, 'Pickled Pantry', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher7.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (8, 'Shoes At Last', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher8.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (9, 'The French Table', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher9.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (10, 'Kingston Adult Education', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher10.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (11, 'Cafe Amici', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher11.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (12, 'The Organic Man', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher12.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (13, 'Margo & Rita', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher13.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (14, 'The Lamb', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher14.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (15, 'Surbiton Veterinary Hospital', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher15.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (16, 'Black Lion', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher16.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (17, 'C.D Jennings and Sons', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher17.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (18, 'Gordon Bennetts', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher18.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (19, 'YMCA Surbiton', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher19.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (20, 'La Crepe des Delices', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher20.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (21, 'The Copper Kettle', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher21.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (22, 'Holiday Inn', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher22.gif');
+INSERT INTO `foodfest`.`voucher` (`POSITION`, `TITLE`, `URL`) VALUES (23, 'The Royal Oak', 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-725924976627/vouchers/voucher23.gif');
+
 
 INSERT INTO `foodfest`.`trader_feed` (`ID`, `KPOUND`) VALUES (1376872062546481, 1);
 INSERT INTO `foodfest`.`trader_feed` (`ID`, `KPOUND`) VALUES (419705668122682, 1);
